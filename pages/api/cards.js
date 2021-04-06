3ca762e489bd903b54928700d71d6a46cd83d9db
John Manning <john.manning144@gmail.com>
2021-04-06 16:19:20 -0400

    added comments to explain swapping between collections

--- pages/api/cards.js
+++ pages/api/cards.js
@@ -7,7 +7,11 @@ export default async (req, res) => {
   
   console.log("connected!!")
   const cards = await db
+    //("cards") is the old, bad collection. Use it for testing ties.
+    // If you do, you need to change "lobbyCards in server/server.js" to be 100, 
+    //there aren't enough cards in the bad dataset.
 
+    //.collection("cards")
     .collection("seoker_cards")
 
     .find({})
