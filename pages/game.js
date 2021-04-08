43515add022d6c7415067e6c775b78d9fba6dcd3
Conner Kulbaski <connerkulbaski@gmail.com>
2021-04-08 16:31:29 -0500

    change "Pot amount: x" to "x chips in the pot

--- pages/game.js
+++ pages/game.js
@@ -411,7 +411,7 @@ class GameScreen extends React.Component {
                 <div className={gameSty.gameroom}>
                 
                 <div className={gameSty.gameroomL}> 
-                    <h3>Pot amount: {this.state.gameInfo.potAmount}</h3> 
+                    <h3>{this.state.gameInfo.potAmount} chips in the pot</h3> 
                     
                     {this.printPlayers()}
 
